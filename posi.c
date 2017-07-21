16ce15c3ffcbf70168cf65500d920edb520ee9e0
arunvsb <30343648+arunvsb@users.noreply.github.com>
2017-07-21 17:18:28 +0530

    Create posi.c

---
+++ posi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n;
+clrscr();
+scanf("%d",&n);
+printf("%d",n);
+if(n<0)
+{
+printf("the given number is positive");
+}
+else
+{
+printf("the given number is negative");
+}
+getch();
+}
