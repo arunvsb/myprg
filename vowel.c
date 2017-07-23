0fe6125d5da95d3f7fc65defd45b947a63a89d9a
arunvsb <30343648+arunvsb@users.noreply.github.com>
2017-07-23 10:58:37 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,10 +3,15 @@ void main()
 {
 char ch;
 printf("enter the character\n");
-scanf("%d",&ch);
+scanf("%c",&ch);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-printf("%c is a vowel\n",ch);
+{
+    printf("%c is a vowel\n",ch);
+}
+
 else
+{
 printf("%c is not a vowel\n",ch);
-getch();
+}
+
 }
