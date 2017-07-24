d4bbfc086a86f031d8575bd4949a3bbded9dde7d
arun <30343648+arunvsb@users.noreply.github.com>
2017-07-24 10:27:52 +0530

    Update primeinterval.c

--- primeinterval.c
+++ primeinterval.c
@@ -2,10 +2,10 @@
 int main()
 {
     int low, high, i, f;
-    printf("Enter two numbers(intervals): ");
+    printf("Enter two numbers(intervals) ");
     scanf("%d %d", &low, &high);
 
-    printf("Prime numbers between %d and %d are: ", low, high);
+    printf("Prime numbers between %d and %d are ", low, high);
 
     while (low < high)
     {
